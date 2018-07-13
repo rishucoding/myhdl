4a1168f7e4fef701db8737c7f8e739fbdbcd1cf1
rishucoding <rishucoding@gmail.com>
2018-07-13 10:56:02 +0530

    added documentation and custom clock code

--- my_prac_tut/param.py
+++ my_prac_tut/param.py
@@ -2,13 +2,13 @@ from myhdl import always, Signal, delay, instance, block
 
 @block
 def clkDriver(clk, period = 20):
-
+	# clk is a asignal parameter
 	lowTime = int(period/2)
 	highTime = period - lowTime
 
 	@instance
-	def drive_clk():
-		while(True):
+	def drive_clk(): # explicitily define the desired behaviour
+		while(True): # for generator to run forever
 			yield delay(lowTime)
 			clk.next = 1
 			yield delay(highTime)
